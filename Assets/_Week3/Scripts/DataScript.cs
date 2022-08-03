a35bd138050bd3a26a47a4693493b8caa218eddc
Lachlan Sleight <lachlansleight@gmail.com>
2022-08-03 11:12:47 +1000

    feat: Stuff we did in-class during week 3

---
+++ Assets/_Week3/Scripts/DataScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DataScript", menuName = "DataScript", order = 100)]
+public class DataScript : ScriptableObject
+{
+    public float Speed;
+}
---
+++ Assets/_Week3/Scripts/Editor/ShelfSpawnerEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ShelfSpawner))]
+public class ShelfSpawnerEditor : Editor
+{
+
+    private SerializedProperty _gap;
+
+    public void OnEnable()
+    {
+        _gap = serializedObject.FindProperty("Gap");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        var shelfSpawner = (ShelfSpawner)target;
+        base.OnInspectorGUI();
+
+        //shelfSpawner.Gap = EditorGUILayout.Slider(shelfSpawner.Gap, 0f, 1f);
+        EditorGUILayout.PropertyField(_gap, new GUIContent("Gap"));
+
+        if (GUILayout.Button("Spawn Object"))
+        {
+            shelfSpawner.TestSpawn();
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
