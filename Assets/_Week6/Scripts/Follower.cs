a63420ab1329032be0a9573ceb651a6424b8cad5
Lachlan Sleight <lachlansleight@gmail.com>
2022-08-22 15:56:28 +1000

    fix: Oops spoilers

--- Assets/_Week6/Scripts/Follower.cs
+++ Assets/_Week6/Scripts/Follower.cs
@@ -6,17 +6,8 @@ public class Follower : MonoBehaviour
 {
     public Transform Target;
 
-    public float LerpSpeed = 4f;
-    public float MaxSpeed = 4f;
-    public float SmoothTime = 2f;
-    
-    private Vector3 _velocity;
-
     public void Update()
     {
-        //transform.position = Vector3.Lerp(transform.position, Target.position, Time.deltaTime * LerpSpeed);
-        //transform.position = Vector3.MoveTowards(transform.position, Target.position, MaxSpeed * Time.deltaTime);
-        transform.position =
-            Vector3.SmoothDamp(transform.position, Target.position, ref _velocity, SmoothTime, MaxSpeed);
+        transform.position = Target.position;
     }
 }
--- Assets/_Week6/Scripts/Looker.cs
+++ Assets/_Week6/Scripts/Looker.cs
@@ -6,15 +6,10 @@ public class Looker : MonoBehaviour
 {
 
     public Transform Target;
-    public float LerpRate = 4f;
-    public float RotateRate = 90f;
 
     public void Update()
     {
-        var targetForward = Vector3.RotateTowards(transform.forward, Target.position - transform.position, RotateRate * Time.deltaTime * Mathf.Deg2Rad, 0f);
+        var targetForward = Target.position - transform.position;
         transform.rotation = Quaternion.LookRotation(targetForward, Vector3.up);
-        return;
-        var targetRotation = Quaternion.LookRotation(Target.position - transform.position, Vector3.up);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * LerpRate);
     }
 }
